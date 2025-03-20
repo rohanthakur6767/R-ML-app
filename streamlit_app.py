149a61c48f16c6db2e657ee3f01538a8906560a9
Rohan Thakur <rt633811@gmail.com>
2025-03-20 10:11:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,27 +7,17 @@ from PIL import Image
 st.set_page_config(page_title="Happy Birthday 🎂", layout="centered")
 
 # Title
-st.title("🎉 Happy Birthday, Anshikaa.. 🎈")
+st.title("🎉 Happy Birthday,  🎈")
 
 # Display a special message
 st.subheader("💖 A Special Message Just for You 💖")
 
 # Birthday Message
 message = """  
-My love Anshikaa,  
 
-happy birthday to my love of my life...
 
-Meri jaan, tumse milne ka intezaar ab aur nahi ho raha! 💖 
-Tumhari hasi meri duniya roshan karti hai, aur tumhari baatein mere dil ka sukoon hain.
-tum mere liye bhot special ho i love you soo much my love 🌸✨  
+  
 
-i promise i will keep loving youu darling..
-
-Have the best birthday ever! 🎈🎁🎊  
-
-With lots of love,  
-your Rohan Thakur
 """
 if st.button("Click to Reveal the Surprise 💌"):
     with st.spinner("Unwrapping the surprise... 🎁"):
@@ -44,7 +34,7 @@ def resize_to_square(image_path, size=300):
 st.subheader("📸 Some Beautiful Pictures 📸")
 
 # List of image paths
-image_paths = ["2.jpg", "3.jpg", "4.jpg"]
+image_paths = [".jpg", ".jpg", ".jpg"]
 
 # Display resized images
 for img_path in image_paths:
